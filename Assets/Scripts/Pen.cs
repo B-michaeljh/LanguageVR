721912545932dc354a23c677c1c20ad370007cc9
cs17bjm <1624079@brunel.ac.uk>
2021-01-29 17:47:48 +0000

    Implemented Caligraphy Room

---
+++ Assets/Scripts/Pen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pen : MonoBehaviour
+{
+    public GameObject Brush;
+    public float inkRate = 0.05f;
+    float nextInk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 14 && nextInk < Time.time)
+        {
+            Vector3 SpawnHere = new Vector3(transform.position.x, transform.position.y, other.transform.position.z);
+            //Debug.Log("Trigger On Layer 13");
+            nextInk = Time.time + inkRate;
+            Instantiate(Brush, SpawnHere, other.transform.rotation * Quaternion.Euler(0f, 0f, 0f));
+        }
+    }
+}
---
+++ Assets/Scripts/Teleport_Caligraphy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Teleport_Caligraphy : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+        //Debug.Log("TRIGGERED");
+
+    }
+}
